c4d3a497c656c620b6c128db745c906cacde3076
Leonardo Hernández Hernández <leohdz172@proton.me>
2024-06-22 12:13:28 -0600

    also skip frames if a floating client has a pending resize

--- dwl.c
+++ dwl.c
@@ -2056,7 +2056,7 @@ rendermon(struct wl_listener *listener, void *data)
 	/* Render if no XDG clients have an outstanding resize and are visible on
 	 * this monitor. */
 	wl_list_for_each(c, &clients, link) {
-		if (c->resize && !c->isfloating && client_is_rendered_on_mon(c, m) && !client_is_stopped(c))
+		if (c->resize && client_is_rendered_on_mon(c, m) && !client_is_stopped(c))
 			goto skip;
 	}
 
